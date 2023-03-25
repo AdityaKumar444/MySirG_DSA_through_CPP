7d93974b63c2b9b3da067d40f8d0b52f3c027dbf
Aditya Kumar Gupta <109333726+AdityaKumar444@users.noreply.github.com>
2023-03-25 09:47:45 +0530

    Update BFSGraphMatrix.cpp

--- Assignment - 21 : Graph Traversing/BFSGraphMatrix.cpp
+++ Assignment - 21 : Graph Traversing/BFSGraphMatrix.cpp
@@ -1,27 +1,27 @@
 #include"GraphMatrix.cpp"
 void BFS(Graph &,int);
-void BFS(Graph &g , int source)
+void BFS(Graph &g , int source)       // passing graph and source node
 {
-    queue<int> q ;
-    q.push(source);
-    bool status[g.totalVertices()] ;
-    for(int i=0 ; i<g.totalVertices() ; i++)
-        status[i] = 0 ;
-    status[source] = true ;
+    queue<int> q ;              // creating queue to store vertexes 
+    q.push(source);             // inserting the visited vertex
+    bool status[g.totalVertices()] ;        // creating boolean array for visited and unvisited vertices
+    for(int i=0 ; i<g.totalVertices() ; i++)   // setting all vertices unvisited initially 
+        status[i] = false ;
+    status[source] = true ;     // setting source vertex visited
     int n , u ;    
-    while(!q.empty())
+    while(!q.empty())       // iterating until queue becomes empty
     {
-        n = q.front();
+        n = q.front();      // taking out front element of queue to find adjacent vertices to it 
         cout << "V" << n << " ";
-        q.pop();
-        vector<int> adjacent = g.getAdjacent(n);
-        for(int j=0 ; j<adjacent.size() ; j++)
+        q.pop();            // poping from queue
+        vector<int> adjacent = g.getAdjacent(n); // getting all adjacent vertices of n and storing in dynamic array
+        for(int j=0 ; j<adjacent.size() ; j++) // iterating until all adjacent vertices are visited
         {
-            u = adjacent[j];
-            if(status[u]==false)
+            u = adjacent[j];        // going to first adjacent vertex to n 
+            if(status[u]==false)    // checking if that adjacent vertex is visited or not
             {
-                q.push(u);
-                status[u] = true ;
+                q.push(u);          // if not visited pushing it in queue
+                status[u] = true ;  // updating adjacent vertex status to visited
             }
         }
     }
